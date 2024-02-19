eac9ed004e7e26dd029591a9da4418a9aa0e46cd
Paulo H. Lamounier <53798700+Nanashii76@users.noreply.github.com>
2024-02-19 10:24:19 -0300

    Last beginner contest Atcoder

---
+++ Atcoder/abc341/A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n ; ++i)
+        cout << "10";
+
+    cout << "1" << endl;
+
+}
