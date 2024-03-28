8fdc4c828354975cee1e6e4dcfd6c61138c2c9b6
Paulo H. Lamounier <53798700+Nanashii76@users.noreply.github.com>
2024-03-28 13:14:47 -0300

    adding one more question from cses

--- cses/introductory_problems/number_spiral.cpp
+++ cses/introductory_problems/number_spiral.cpp
@@ -1,5 +1,44 @@
 #include <bits/stdc++.h>
 
+typedef long long ll;
+
+void solve(ll r, ll c) {
+
+    ll result = 0;
+
+    // finding the diagonal
+    if(r == c)
+        result = (r*c) - (r-1);
+    
+    if(r > c) {
+        if(r&1) {
+            result = ((r*r)-(r-1)) - (r-c);
+        } else {
+            result = ((r*r)-(r-1)) + (r-c);
+        }
+    }
+
+    if(c > r) {
+        if(c&1) {
+            result = ((c*c)-(c-1)) + (c-r);
+        } else {
+            result = ((c*c)-(c-1)) - (c-r);
+        }
+    }
+
+    std::cout << result << std::endl;
+
+}
+
 int main(void) {
 
+    int t; std::cin >> t;
+    ll r,c;
+    for(int i = 0; i < t; ++i) {
+        std::cin >> r >> c;
+        solve(r,c);
+    }
+
+    return EXIT_SUCCESS;
+    
 }
