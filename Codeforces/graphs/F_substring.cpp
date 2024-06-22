f67e0d8396ed844ad8d2010bc22ff95fc56eaa38
Paulo H. Lamounier <53798700+Nanashii76@users.noreply.github.com>
2024-06-22 00:32:18 -0300

    Reviewing F

--- Codeforces/graphs/F_substring.cpp
+++ Codeforces/graphs/F_substring.cpp
@@ -7,29 +7,39 @@ vector<vector<int>> g(N);
 vector<int> toposort;
 
 int main() {
-
     ios_base::sync_with_stdio(false); 
     cin.tie(NULL);    
 
-    int n, m; cin >> n >> m;
-    string str; cin >> str;
+    int n, m; 
+    cin >> n >> m;
+    string str; 
+    cin >> str;
+
+    vector<vector<int>> freq(n, vector<int>(26, 0)); 
+
     while(m--) {
-        int u,v; cin >> u >> v;
-        --u,--v;
+        int u, v; 
+        cin >> u >> v;
+        --u, --v;
         g[u].emplace_back(v);
         ++indegree[v];
     }
 
     queue<int> q;
-    int count = 0;
     for(int i = 0; i < n; ++i) {
-        if(not indegree[i])
+        if(not indegree[i]) {
             q.push(i);
+            freq[i][str[i] - 'a'] = 1; 
+        }
     }   
 
+    int count = 0;
+    int maxVal = 0;
+
     while(not q.empty()) {
+        int x = q.front(); 
+        q.pop();
         ++count;
-        int x = q.front(); q.pop();
         toposort.emplace_back(x);
 
         for(auto v : g[x]) {
@@ -37,24 +47,17 @@ int main() {
             if(not indegree[v]) {
                 q.push(v);
             }
+            for (int c = 0; c < 26; ++c) {
+                freq[v][c] = max(freq[v][c], freq[x][c] + (str[v] - 'a' == c ? 1 : 0));
+                maxVal = max(maxVal, freq[v][c]);
+            }
         }
     }
-    
-    if(count < n)
+
+    if(count < n) {
         cout << "-1" << endl; 
-
-    else {
-        
-        unordered_map<char,int> mp;
-        for(auto x : toposort)
-            mp[str[x]]++;
-        
-        int maxVal = 0;
-        for(auto it : mp)
-            maxVal = max(maxVal,it.second);
-
+    } else {
         cout << maxVal << endl;
-    
     }
 
     return 0;
